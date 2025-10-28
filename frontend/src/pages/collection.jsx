be65ae83a7584adb0e3ac8dd4e2c1996cedd6a71
Sownthear Dhanasekaran <sownthear.dhanasekaran@creosen.com>
2025-10-28 16:32:16 +0530

    Updated Collection,Searchbar

--- frontend/src/pages/collection.jsx
+++ frontend/src/pages/collection.jsx
@@ -6,7 +6,7 @@ import ProductItem from '../components/ProductItem';
 
 const collection = () => {
 
-  const {products} = useContext(ShopContext);
+  const {products, search , showSearch} = useContext(ShopContext);
   const [showFilter,setshowFilter] = useState(false);
   const [filterProducts,setFilterProducts] = useState([]);
   const [category,setCategory] = useState([]);
@@ -34,6 +34,11 @@ const collection = () => {
   const applyFilter = () =>{
     
     let productsCopy = products.slice();
+
+    if (showSearch && search) {
+      productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+    }
+
     if (category.length > 0) {
       productsCopy = productsCopy.filter(item => category.includes(item.category));
     }
@@ -67,7 +72,7 @@ const collection = () => {
 
   useEffect(()=>{
       applyFilter();
-  },[category,subCategory])
+  },[category,subCategory,search,showSearch])
 
   useEffect(()=>{
     sortProduct();
