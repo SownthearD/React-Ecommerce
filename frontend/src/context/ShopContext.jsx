d6203192f00478e3fe43aa93d37e9999244264ee
Sownthear Dhanasekaran <sownthear.dhanasekaran@creosen.com>
2026-03-25 18:41:26 +0530

    Updated shopcontext-2

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -11,7 +11,7 @@ const ShopContextProvider = (props) => {
 
     const currency = '$';
     const delivery_fee = 10;
-    const backendUrl = "https://react-ecommerce-backend-seven.vercel.app/";
+    const backendUrl = "https://react-ecommerce-backend-seven.vercel.app";
     // const backendUrl = "http://localhost:4000";
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
